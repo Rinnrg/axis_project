d373461743499e576ee600c121fd8d8f084942c7
Rinnrg <rinogumilang2@gmail.com>
2026-07-21 17:44:48 +0700

    fix icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,9 @@ export const metadata: Metadata = {
   description: 'Aplikasi presensi karyawan CH Alam Juanda',
   icons: {
     icon: [
-      { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
-      { url: '/icon-dark-32x32.png',  media: '(prefers-color-scheme: dark)'  },
-      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/axis.svg', type: 'image/svg+xml' },
     ],
-    apple: '/apple-icon.png',
+    apple: '/axis.svg',
   },
 };
 
